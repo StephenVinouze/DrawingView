32d0e90a43fcad14f99b850fc16f2b6ace243a9d
Stephen Vinouze <s.vinouze@gmail.com>
2015-11-21 14:06:43 +0100

    [+] Add color picker

--- sample/src/main/java/com/stephenvinouze/drawingviewsample/MainActivity.java
+++ sample/src/main/java/com/stephenvinouze/drawingviewsample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.stephenvinouze.drawingviewsample;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -8,14 +9,16 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.larswerkman.holocolorpicker.ColorPicker;
 import com.stephenvinouze.drawingview.DrawingView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener, ColorPicker.OnColorChangedListener {
 
     private DrawingView mDrawingView;
     private ImageView mDrawingImageView;
     private Button mClearButton;
     private Button mSubmitButton;
+    private Button mColorButton;
     private TextView mThicknessText;
     private SeekBar mThicknessBar;
 
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mDrawingImageView = (ImageView) findViewById(R.id.drawing_image_view);
         mClearButton = (Button) findViewById(R.id.clear_button);
         mSubmitButton = (Button) findViewById(R.id.submit_button);
+        mColorButton = (Button) findViewById(R.id.color_button);
 
         mThicknessText = (TextView) findViewById(R.id.thickness_value);
 
@@ -36,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mClearButton.setOnClickListener(this);
         mSubmitButton.setOnClickListener(this);
+        mColorButton.setOnClickListener(this);
         mThicknessBar.setOnSeekBarChangeListener(this);
 
         updateThickness((int) mDrawingView.getThickness());
@@ -55,12 +60,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mSubmitButton.setVisibility(View.VISIBLE);
                 mDrawingImageView.setVisibility(View.GONE);
                 mDrawingImageView.setImageBitmap(null);
+                mThicknessText.setVisibility(View.VISIBLE);
+                mThicknessBar.setVisibility(View.VISIBLE);
             }
         }
         else if (v == mSubmitButton) {
             mSubmitButton.setVisibility(View.GONE);
             mDrawingImageView.setVisibility(View.VISIBLE);
             mDrawingImageView.setImageBitmap(mDrawingView.getDrawing());
+            mThicknessText.setVisibility(View.GONE);
+            mThicknessBar.setVisibility(View.GONE);
+        }
+        else if (v == mColorButton) {
+            ColorPicker colorPicker = new ColorPicker(this);
+            colorPicker.setOldCenterColor(mDrawingView.getDrawingColor());
+            colorPicker.setOnColorChangedListener(this);
+
+            new AlertDialog.Builder(this).setView(colorPicker).show();
         }
     }
 
@@ -74,4 +90,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onColorChanged(int color) {
+        mDrawingView.setDrawingColor(color);
+    }
 }
